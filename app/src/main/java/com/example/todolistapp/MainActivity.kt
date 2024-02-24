077c671e06b90dd9e7e93a47e6911279a9f53c51
pr-Dutta <89duttapranjal@gmail.com>
2024-02-24 21:14:02 +0530

    first commit

---
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.todolistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.todolistapp.ui.theme.ToDoListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoListAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ToDoListAppUi("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ToDoListAppUi(name: String, modifier: Modifier = Modifier) {
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            fontSize = 32.sp
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    name = "TO-DO List App",
+    showSystemUi = true
+)
+@Composable
+fun GreetingPreview() {
+    ToDoListAppTheme {
+        ToDoListAppUi("Android")
+    }
+}
