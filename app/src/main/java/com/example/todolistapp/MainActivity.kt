61febda5a8719c955e380a3b60541630578797a5
pr-Dutta <89duttapranjal@gmail.com>
2024-02-25 21:17:30 +0530

    new commit

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -5,14 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.todolistapp.ui.theme.ToDoListAppTheme
 
@@ -40,12 +50,33 @@ fun ToDoListAppUi(name: String, modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier,
-            fontSize = 32.sp
+        Button(onClick = { /*TODO*/ }) {
+            Text(
+                text = "Add Items",
+                fontSize = 18.sp
+            )
+        }
+
+        Spacer(modifier = Modifier.padding(18.dp))
+
+        var text by remember { mutableStateOf(" ") }
+        TextField(
+            value = text,
+            onValueChange = { newText -> text = newText },
+            label = { Text(
+                text = "Enter text here",
+                fontSize = 18.sp) },
         )
     }
+}
+
+@Composable
+fun EditableItem() {
+    var text by remember { mutableStateOf(" ") }
+    TextField(
+        value = text,
+        onValueChange = { newText -> text = newText }
+    )
 }
 
 @Preview(
